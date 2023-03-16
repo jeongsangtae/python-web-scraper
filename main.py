bc41ea713a5da0b4138f323a2b6fc9dbc4b58771
jeongsangtae <wjdtkdxo55@naver.com>
2023-03-17 03:50:17 +0900

    Python - Python method

--- main.py
+++ main.py
@@ -1,62 +1,78 @@
-# def tax_calc(money):
-#   return money * 0.35
+# function + parameter
+"""
+def tax_calc(money):
+  return money * 0.35
 
-# def pay_tax(tax):
-#   print("thank you for paying", tax)
+def pay_tax(tax):
+  print("thank you for paying", tax)
 
-# pay_tax(tax_calc(150000000))
-
-
-
-# my_name = '상태'
-# my_age = 31
-# my_color = 'black'
-
-# print(f"안녕 나는 {my_name} {my_age}살이야 좋아하는 색은 {my_color}")
+pay_tax(tax_calc(150000000))
+"""
 
 
 
-# def make_juice(fruit):
-#   return f"{fruit}+🥤"
+# f()
+"""
+my_name = '상태'
+my_age = 31
+my_color = 'black'
 
-# def add_ice(juice):
-#   return f"{juice}+🧊"
-
-# def add_sugar(iced_juice):
-#   return f"{iced_juice}+🍾"
-
-# juice = make_juice("🥝")
-# cold_juice = add_ice(juice)
-# perfect_juice = add_sugar(cold_juice)
-# print(perfect_juice)
+print(f"안녕 나는 {my_name} {my_age}살이야 좋아하는 색은 {my_color}")
+"""
 
 
 
-# winner = 5
+# return
+"""
+def make_juice(fruit):
+  return f"{fruit}+🥤"
 
-# if winner > 10:
-#   print("winner is greater than 10")
-# elif winner < 10:
-#   print("winner is less than 10")
-# else:
-#   print("winner is 10")
+def add_ice(juice):
+  return f"{juice}+🧊"
 
-# age = int(input("몇살이니?"))
+def add_sugar(iced_juice):
+  return f"{iced_juice}+🍾"
 
-
-
-# if age < 18:
-#   print("You cant drink")
-# elif age > 18 and age < 35:
-#   print('you drink beer!')
-# elif age == 60 or age == 70:
-#   print("birthday party")
-# else:
-#   print("go ahead")
+juice = make_juice("🥝")
+cold_juice = add_ice(juice)
+perfect_juice = add_sugar(cold_juice)
+print(perfect_juice)
+"""
 
 
 
+# if-else-elif
+"""
+winner = 5
 
+if winner > 10:
+  print("winner is greater than 10")
+elif winner < 10:
+  print("winner is less than 10")
+else:
+  print("winner is 10")
+
+age = int(input("몇살이니?"))
+"""
+
+
+
+# And & Or
+"""
+if age < 18:
+  print("You cant drink")
+elif age > 18 and age < 35:
+  print('you drink beer!')
+elif age == 60 or age == 70:
+  print("birthday party")
+else:
+  print("go ahead")
+"""
+
+
+
+# while + Python Standard Library
+"""
 from random import randint
 
 print("파이썬 카지노 어서오세요")
@@ -75,4 +91,17 @@ while playing:
     print("그것보단 낮아")
   elif user_choice < pc_choice:
     print('그것보단 높아')
+"""
 
+
+
+# method
+
+
+
+name = "jeong"
+
+print(name.upper())
+print(name.capitalize())
+print(name.endswith("o"))
+print(name.replace("j", "y"))
