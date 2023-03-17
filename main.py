61164a8c7168ec6bd992da05dae66665eb0d3577
jeongsangtae <wjdtkdxo55@naver.com>
2023-03-17 23:52:22 +0900

    Python - Python Status Code

--- main.py
+++ main.py
@@ -157,17 +157,26 @@ print(player)
 
 
 
-# For Loop
+# For Loop + URL Formatting + Status Code
+from requests import get
+
 websites = (
   "google.com",
   "airbnb.com",
   "https://twitter.com",
   "facebook.com",
-  "https://tiktok.com"
+  "https://tiktok.com",
 )
+
+results = {}
 
 for website in websites:
   if not website.startswith("https://"):
     website = f"https://{website}"
-  print(website)
-  
+  response = get(website)
+  if response.status_code == 200:
+    results[website] = "OK"
+  else:
+    results[website] = "FAILED"
+
+print(results)
